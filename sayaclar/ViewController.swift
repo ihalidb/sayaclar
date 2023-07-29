0359616aa68ca96796a82994477091dd3ab1469f
İbrhaim Halid Bayrak <ibrahimh.bayrak@gmail.com>
2023-07-29 20:26:11 +0300

    timer oluşturuldu. Zamanlayıcı kullanımı öğrenildi.

--- sayaclar/ViewController.swift
+++ sayaclar/ViewController.swift
@@ -8,12 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var label: UILabel!
+    
+    var timer = Timer()
+    var kalanZaman = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        kalanZaman = 15
+        label.text = "Zaman: \(kalanZaman)"
+        
     }
-
-
+    @IBAction func baslatTiklandi(_ sender: Any) {
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerFonksiyonu), userInfo: nil, repeats: true)
+        
+    }
+    
+    @objc func timerFonksiyonu(){
+        
+        label.text = "Zaman: \(kalanZaman)"
+        kalanZaman = kalanZaman - 1
+        if kalanZaman == 0 {
+            label.text = "Süre Bitti!"
+            timer.invalidate()
+            kalanZaman = 15
+        }
+        
+    }
+    
+    
 }
+
+    
+    
 
